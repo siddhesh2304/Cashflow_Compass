7443e25ac81284f7ccd0533894dddd59c569f6ba
devpr <devpratasingh122@gmail.com>
2023-10-28 21:07:21 +0530

    added activity_dashboard.xml

---
+++ app/src/main/java/com/example/cashflowcompass/Dashboard_activity.java
@@ -0,0 +1,14 @@
+package com.example.cashflowcompass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
